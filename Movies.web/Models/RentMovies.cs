38c758673c1b2bb42d302886b46ea2c8bc9efe2b
AlexSoul1903 <alexmanuel19frias@gmail.com>
2022-10-19 11:05:13 -0400

    ModelsForSalesAndRent2

---
+++ Movies.web/Models/RentMovies.cs
@@ -0,0 +1,13 @@
+﻿namespace Movies.web.Models
+{
+    public class RentMovies
+    {
+        public int Id { get; set; }
+        public int Client_id { get; set; }
+        public int Movie_id { get; set; }
+        public double RentPrice { get; set; }
+        public DateOnly RentDate { get; set; }
+        public DateOnly ExpirationDate { get; set; }
+       }
+    }
+
