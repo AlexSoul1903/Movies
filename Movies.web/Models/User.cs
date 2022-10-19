ba4e2f0515cc7567a6a8aadb6e09db7c0ff587d6
David <raymondinf23@gmail.com>
2022-10-19 01:15:13 -0400

    Add build changes

---
+++ Movies.web/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Movies.web.Models
+{
+
+    public class User
+    {
+
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password{  get; set;}
+
+    }
+
+
+}
