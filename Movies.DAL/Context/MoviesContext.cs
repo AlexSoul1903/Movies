ef22cca4d45ac96459d2905e527fd53061a3c136
AlexSoul1903 <alexmanuel19frias@gmail.com>
2022-10-24 17:11:21 -0400

    DAL Estructure.

---
+++ Movies.DAL/Context/MoviesContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.DAL.Context
+{
+    public partial class MoviesContext : DbContext
+    {
+
+        public MoviesContext()
+        {
+
+
+
+        }
+
+        public MoviesContext(DbContextOptions<MoviesContext> options):
+
+            base(options)
+        {
+
+
+        }
+
+
+
+        #region "entities"
+
+
+
+
+
+
+
+        #endregion
+    }
+}
