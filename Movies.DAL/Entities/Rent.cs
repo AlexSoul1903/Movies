9fc64be627d4adccfbbbc5307f524c32f22564ac
Kristyn Elizabeth Herrera Felix <20211966@itla.edu.do>
2022-10-28 20:41:58 -0400

    Added more things

--- Movies.DAL/Entities/Rent.cs
+++ Movies.DAL/Entities/Rent.cs
@@ -8,7 +8,6 @@ namespace Movies.DAL.Entities
 {
     public class Rent : Core.BaseEntity
     {
-        public int ID { get; set; }
         public int ClientID { get; set; }
         public double RentPrice{ get; set; }
         public DateTime RentDate{ get; set; }
