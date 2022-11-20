e963f8dd9f6d5071dfaa090305e4e223bcfb16df
Ferlin Miguel Feliz Guerrero <ferlin_18_feliz@hotmail.com>
2022-11-19 20:45:08 -0400

    Ok.

---
+++ Movies.Service/Validations/ValidationsPayment.cs
@@ -0,0 +1,34 @@
+﻿using Movies.Service.Core;
+using Movies.Service.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Service.Validations
+{
+    public class ValidationsPayment
+    {
+        public static ServiceResult IsValidPayment(PaymentDto payment)
+        {
+            ServiceResult result = new ServiceResult();
+
+            if (string.IsNullOrEmpty(payment.OwnerName))
+            {
+                result.Success = false;
+                result.Message = "Owner name is required";
+                return result;
+            }
+            
+            if (payment.OwnerName.Length >30 )
+            {
+                result.Success = false;
+                result.Message = "Owner name length is not valid.";
+                return result;
+            }
+            return result;
+        }
+
+    }
+}
